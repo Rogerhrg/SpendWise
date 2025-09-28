2001ac1a31a1e9ba334fff76358c544c41cf0eba
Rogelio <roger.hrg@gmail.com>
2025-09-28 10:24:27 -0600

    Database Conection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,18 @@
 import express from 'express' 
 import colors from 'colors'
 import morgan from 'morgan'
+import { db } from './config/db'
+
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log(colors.blue.bold('Conexión a BD exitosa'))
+    } catch (error) {
+        console.log(colors.blue.bold('Conexión a BD fallida'))
+    }
+}
+connectDB()
 
 const app = express()
 
