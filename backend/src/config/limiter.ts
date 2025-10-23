c3559393b977898c145011ec50e296078dfa4102
Rogelio De La Rosa <roger.hrg@gmail.com>
2025-10-23 11:57:10 -0600

    Preparación para Front End

--- backend/src/config/limiter.ts
+++
@@ -1,9 +0,0 @@
-import rateLimit from "express-rate-limit"
-
-export const limiter = rateLimit({
-    windowMs: 60 * 1000, // 15 minutes
-    max: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    message: {"error": "Has excedido el límite de solicitudes. Por favor, inténtalo de nuevo más tarde."},
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
--- backend/src/utils/jwt.ts
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-export const generateJWT = (id: string) => {
-    const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '30d'
-    })
-    return token
-}
