f7a6bb529b1e70beea37921a0e8d918308b22e78
Rogelio De La Rosa <roger.hrg@gmail.com>
2025-10-30 00:35:18 -0600

    AuthLayout

--- frontend/app/auth/forgot-password/page.tsx
+++ frontend/app/auth/forgot-password/page.tsx
@@ -1,14 +1,14 @@
 import RegisterForm from "@/components/auth/RegisterForm";
 
 export const metadata = {
-  title: "SpendWise - Crear Cuenta",
-  description: "Crea una cuenta para administrar tus finanzas",
+  title: "SpendWise - Olvidé mi Contraseña",
+  description: "Cambia tu contraseña olvidada",
 };
 
-export default function RegisterPage() {
+export default function ForgotPasswordPage() {
   return (
     <>
-      <h1 className="font-black text-6xl text-purple-950">Crear Cuenta</h1>
+      <h1 className="font-black text-6xl text-purple-950">Olvidé mi Contraseña</h1>
       <p className="text-3xl font-bold">
         y controla tus <span className="text-amber-500">finanzas</span>
       </p>
--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -1,14 +1,14 @@
 import RegisterForm from "@/components/auth/RegisterForm";
 
 export const metadata = {
-  title: "SpendWise - Crear Cuenta",
-  description: "Crea una cuenta para administrar tus finanzas",
+  title: "SpendWise - Iniciar Sesión",
+  description: "Comienza a administrar tus finanzas",
 };
 
-export default function RegisterPage() {
+export default function LoginPage() {
   return (
     <>
-      <h1 className="font-black text-6xl text-purple-950">Crear Cuenta</h1>
+      <h1 className="font-black text-6xl text-purple-950">Iniciar Sesión</h1>
       <p className="text-3xl font-bold">
         y controla tus <span className="text-amber-500">finanzas</span>
       </p>
